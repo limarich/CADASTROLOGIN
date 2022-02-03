9a8079c1f0d66538b2963018813ca0f0041df208
Richard <59461895+limarich@users.noreply.github.com>
2022-02-03 16:41:15 -0300

    configura celebrate

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express, { json } from "express";
 import { sequelize } from "./db/db";
-import {router} from "./routes/router";
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+import { router } from "./routes/router";
+import path = require("path");
+import { errors } from "celebrate";
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 
-app.use(json())
-app.use(router)
-
-
-app.listen(3000, async ()=> {
-  await sequelize.sync() 
-  console.log(`App rodando na porta 3000`)
-}) 
+app.use(json());
+app.use(router);
+app.use(errors());
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`App rodando na porta 3000`);
+    });
+  })
+  .catch((err) => console.log(err));
