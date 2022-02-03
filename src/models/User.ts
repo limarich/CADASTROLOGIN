2dcebb307a45e35ebee4478e9ac33f2252e596d3
Richard <59461895+limarich@users.noreply.github.com>
2022-02-02 22:19:24 -0300

    define o model do user

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import {sequelize} from "../db/db";
+
+export const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement:true,
+      allowNull: false,
+      primaryKey:true
+    }, 
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, 
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, 
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, 
+})
