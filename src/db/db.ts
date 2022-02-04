1a65161bbce3c1608da04d053374b21d5ea8a69d
Richard <59461895+limarich@users.noreply.github.com>
2022-02-04 01:34:26 -0300

    configura variaveis de ambiente

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,18 +1,28 @@
-import {Sequelize} from "sequelize";
-const {DATABASE_NAME,DATABASE_HOST,DATABASE_PORT,DATABASE_USER,DATABASE_PASSWORD} = process.env;
+import { Sequelize } from "sequelize";
+const {
+  DATABASE_NAME,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+} = process.env;
 
-// const sequelize = new Sequelize(`${DATABASE_NAME}`, `${DATABASE_USER}`, `${DATABASE_PASSWORD}`, {
-//   host: `${DATABASE_HOST}`,
-//   dialect: 'mysql',
-//   port:  3306
-// })
-const sequelize = new Sequelize(`sistema`, `root`, ``, {
-  host: `localhost`,
-  dialect: 'mysql',
-  port:  3306
-})
+const sequelize = new Sequelize(
+  `${DATABASE_NAME}`,
+  `${DATABASE_USER}`,
+  `${DATABASE_PASSWORD}`,
+  {
+    host: `${DATABASE_HOST}`,
+    dialect: "mysql",
+    port: +`${DATABASE_PORT}`,
+  }
+);
 sequelize
-.authenticate()
-.then(()=>{console.log("Conexão com db efetuada com sucesso")})
-.catch((err)=>{console.log("ERRO: Não foi possível efetuar a conexão com o db!\n\n", err)})
-export {sequelize}
+  .authenticate()
+  .then(() => {
+    console.log("Conexão com db efetuada com sucesso");
+  })
+  .catch((err) => {
+    console.log("ERRO: Não foi possível efetuar a conexão com o db!\n\n", err);
+  });
+export { sequelize };
